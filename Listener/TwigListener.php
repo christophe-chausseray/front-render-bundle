748f671345fed5afb9a54915a2b56d562e6d8b5d
Christophe Chausseray <christophec@iron-mail.net>
2016-02-10 11:44:17 +0000

    [feature/add-exception-listener] Update method name and PHPDoc

--- Listener/TwigListener.php
+++ Listener/TwigListener.php
@@ -2,13 +2,14 @@
 
 namespace Chris\Bundle\FrontRenderBundle\Listener;
 
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Twig_Environment;
 use Twig_Lexer;
 
 class TwigListener
 {
     /**
-     * @var Twig_Environment $twig
+     * @var Twig_Environment
      */
     protected $twig;
 
@@ -21,18 +22,22 @@ class TwigListener
     }
 
     /**
+     * Set the custom twig lexer to display custom tags on the front
+     *
      * Update twig tags
      */
-    public function updateTagTwig()
+    public function updateTagTwig(GetResponseEvent $event)
     {
-        $lexer = $this->getLexer();
+        $lexer = $this->getFrontLexer();
         $this->twig->setLexer($lexer);
     }
 
     /**
+     * Retrieve the custom twig lexer for the front
+     *
      * @return Twig_Lexer
      */
-    public function getLexer()
+    public function getFrontLexer()
     {
         $lexer = new Twig_Lexer($this->twig, [
             'tag_comment'  => ['{*', '*}'],
